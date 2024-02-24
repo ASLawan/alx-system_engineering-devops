80254e2446389f56a0f0cbbc7813d9bc885ca53d
ASLawan <nawalnitsua@gmail.com>
2024-02-24 22:48:59 +0100

    Provide correct file name

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # Configure shh client to use private key for authentication
 exec { 'echo':
   path    => 'usr/bin:/bin',
-  command => 'echo "  IdentityFile ~/.shh/school\n  PasswordAuthentication no" >> /etc/ssh/shh_config',
+  command => 'echo "  IdentityFile ~/.shh/school\n  PasswordAuthentication no" >> /etc/ssh/ssh_config',
   returns => [0, 1],
 }
