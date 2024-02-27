1c14c97dbb9ea6e2d5f1e8bb7576a004e0017d55
ASLawan <nawalnitsua@gmail.com>
2024-02-27 11:56:48 +0100

    Correct code output errors

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,8 +2,8 @@
 
 exec { 'install':
   provider => shell,
-  command  => 'sudo apt-get -y update; sudo apt-get install -y nginx; echo "Hello World!" > | sudo tee /var/www/html/index.html; 
-sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/ASLawan permanent;/" 
-/etc/nginx/sites-available/default; /etc/init.d/nginx restart',
+  command  => 'sudo apt-get -y update ; sudo apt-get install -y nginx ; echo "Hello World!" > | sudo tee /var/www/html/index.html ;
+	       sudo sed -i "s/server_name _;/server_name _ ;\n\trewrite ^\/redirect_me https:\/\/github.com\/ASLawan permanent;/" 
+	       /etc/nginx/sites-available/default ; sudo service nginx start',
 }
 
