f09fb38aff0db8412047f61a35986c17126f7516
ASLawan <nawalnitsua@gmail.com>
2024-02-27 11:51:51 +0100

    Write a puppet manifest to configure nginx server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,9 @@
+# script to install nginx web server using puppet
+
+exec { 'install':
+  provider => shell,
+  command  => 'sudo apt-get -y update; sudo apt-get install -y nginx; echo "Hello World!" > | sudo tee /var/www/html/index.html; 
+sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/ASLawan permanent;/" 
+/etc/nginx/sites-available/default; /etc/init.d/nginx restart',
+}
+
