e8dfe705504c618fd2aacd762281b58c6c85b1ed
ASLawan <nawalnitsua@gmail.com>
2024-02-27 23:45:49 +0100

    Fix code output error

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,7 +2,7 @@
 
 exec { 'install':
   provider => shell,
-  command  => 'sudo apt-get -y update ; sudo apt-get install -y nginx ; echo "Hello World!" > | sudo tee /var/www/html/index.html ;
+  command  => 'sudo apt-get -y update ; sudo apt-get install -y nginx ; echo "Hello World!" | sudo tee /var/www/html/index.html ;
 	       sudo sed -i "s/server_name _;/server_name _ ;\n\trewrite ^\/redirect_me https:\/\/github.com\/ASLawan permanent;/" 
 	       /etc/nginx/sites-available/default ; sudo service nginx start',
 }
