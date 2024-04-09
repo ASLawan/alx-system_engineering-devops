8a82b96fc0b7d0ee2a42675ef643a413c83cbfa1
ASLawan <nawalnitsua@gmail.com>
2024-04-09 18:58:46 +0100

    Return total number of subscribers for a given subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+    Module implementing a function that returns
+    number of total subscribers for a given subreddit
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """returns total number of subscribers"""
+    u_agent = "Mozilla/5.0"
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+            "User-Agent": "u_agent"
+        }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        return 0
+    dict = response.json()
+    if "data" not in dict:
+        return 0
+    if "subscribers" not in dict.get('data'):
+        return 0
+    subscribers = dict['data']['subscribers']
+    return subscribers
