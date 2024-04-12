959a533403583973406e029a3c3b5f2a425ae313
ASLawan <nawalnitsua@gmail.com>
2024-04-12 08:39:00 +0100

    Make file puppet--lint compliant

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,4 @@
+# fix Apache server 500 error on a wordpress
 exec { 'replace_file_extensions':
   command => 'sed -i "s/\.phpp/.php/g" /var/www/html/wp-settings.php',
   path    => ['/bin', '/usr/bin'],
